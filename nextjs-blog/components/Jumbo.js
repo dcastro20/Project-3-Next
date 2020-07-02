573dd9c3ca7a5f495195bb561a4e8c3b61f7e70d
John_Romano <romanoj723@gmail.com>
2020-07-02 14:22:27 -0400

    Added Footer component

--- nextjs-blog/components/Jumbo.js
+++ nextjs-blog/components/Jumbo.js
@@ -3,7 +3,7 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 const Jumbo = () => (
   <Jumbotron fluid>
     <h1>Plan-it-Fitness</h1>
-    <img src="/images/fitness2.jpg" fluid alt="Logo" />
+    <img src="/images/fitness2.jpg" fluid="true" alt="Logo" />
   </Jumbotron>
 );
 
