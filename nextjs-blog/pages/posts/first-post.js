df890f3f47ad3cc5dae70f0c97f945bd552bc44b
Doris Castro <62316845+dcastro20@users.noreply.github.com>
2020-07-06 14:51:02 -0400

    Update first-post.js

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Layout from '../../components/layout'
+import Layout from '../../components/Layout'
 
 export default function FirstPost() {
     return (
