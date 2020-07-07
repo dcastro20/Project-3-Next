21ec0b92c582ac415680d78b3e34c7923e5aeeef
Jon Kwan <iamjonkwan@gmail.com>
2020-07-06 21:37:41 -0400

    updated searchBar component and now successfully renders youtube videos

--- nextjs-blog/components/searchBar.js
+++ nextjs-blog/components/searchBar.js
@@ -1,64 +1,77 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import axios from "axios";
+import React, { useState } from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import YTSearch from "youtube-api-search";
-const API_KEY = "AIzaSyCiZbQgW_osNuaQNxXcFwk_3yuo6_kL7dc";
+// import Youtube from "react-youtube";
 
-class Searchbar extends React.Component {
-  state = {
-    term: "",
-  };
-  handleChange = (event) => {
+const API_KEY = "AIzaSyBvyTA_8gxKEaJ4ugET0uWiyhXrhIdxpDo";
+
+function Searchbar() {
+  const [term, setTerm] = useState("");
+  const [videos, setVideos] = useState([]);
+
+  const handleChange = (event) => {
     console.log("handleChange");
-    this.setState({
-      term: event.target.value,
-    });
-    console.log(this.state.term);
+    setTerm(event.target.value);
+    console.log(term);
   };
-  handleSubmit = (event) => {
+
+  const handleSubmit = (event) => {
     console.log("handleSubmit");
     event.preventDefault();
-    console.log(this.state.term);
-    YTSearch({ key: API_KEY, term: this.state.term, amount: 20 }, (data) => {
+    console.log(term);
+    YTSearch({ key: API_KEY, term: term }, (data) => {
       console.log(data);
+      createRow(data);
     });
-    // axios
-    //   .get({
-    //     baseURL: "https://www.googleapis.com/youtube/v3/",
-    //     params: {
-    //       part: " snippet",
-    //       maxResults: 5,
-    //       q: this.state.term,
-    //       key: KEY,
-    //     },
-    //   })
-    //   .then((res) => {
-    //     console.log("axios res", res);
-    //   })
-    //   .catch((err) => {
-    //     console.log("axios error", err);
-    //   });
   };
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.handleSubmit} className="ui form">
-          <div className="field">
-            <label htmlFor="video-search">Video Search: </label>
-            <input
-              onChange={this.handleChange}
-              name="video-search"
-              type="text"
-              value={this.state.term}
-            />
-          </div>
-          <Button type="submit" variant="success">
-            Search
-          </Button>
-        </form>
-      </div>
-    );
-  }
+  const createRow = (data) => {
+    console.log("createRow");
+    let videos = data.map((video) => {
+      console.log("video.id.videoId", video.id.videoId);
+    });
+    setVideos(data);
+  };
+
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={handleSubmit} className="ui form">
+        <div className="field">
+          <label htmlFor="video-search">Video Search: </label>
+          <input
+            onChange={handleChange}
+            name="video-search"
+            type="text"
+            value={term}
+          />
+        </div>
+        <Button type="submit" variant="success">
+          Search
+        </Button>
+
+        <div>
+          {videos.length ? (
+            videos.map((video) => {
+              console.log("youtube video.id.videoId", video.id.videoId);
+              const url = `https://www.youtube.com/embed/${video.id.videoId}`;
+              console.log("URL: ", url);
+              return (
+                <iframe
+                  width={560}
+                  height={315}
+                  src={url}
+                  frameBorder={0}
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                />
+              );
+            })
+          ) : (
+            <p> </p>
+          )}
+        </div>
+      </form>
+    </div>
+  );
 }
 export default Searchbar;
